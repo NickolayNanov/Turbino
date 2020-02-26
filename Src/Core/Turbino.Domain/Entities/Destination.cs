20deaa338bce19bd10f78cf1a714dc62bc6702ab
Nikolay Nanov <NNanov@jackhenry.com>
2020-02-27 00:49:47 +0200

    Added Layout

--- Src/Core/Turbino.Domain/Entities/Destination.cs
+++ Src/Core/Turbino.Domain/Entities/Destination.cs
@@ -16,6 +16,14 @@
 
         public int TimesVisited { get; set; }
 
+        public string ImgUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string LanguageSpoken { get; set; }
+
+        public int SquareArea { get; set; }
+
         public virtual ICollection<Tour> Tours { get; private set; }
 
         public virtual ICollection<UserDestination> DestionationUsers { get; private set; }
---
+++ Src/Core/Turbino.Domain/Entities/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace Turbino.Domain.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using Turbino.Domain.Entities.Common;
+
+
+    public class Post : BaseDeletableModel
+    {
+        public Post()
+        {
+            this.PublishedOn = DateTime.Now;
+        }
+
+        public string Title { get; set; }
+
+        public DateTime PublishedOn { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; private set; }
+    }
+}
