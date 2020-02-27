87f7d35cba557623b93545e1850cf970e9d797d3
Nikolay <NNanov@jackhenry.com>
2020-02-27 12:52:59 +0200

    added tours page

---
+++ Src/Presentation/Turbino.WebApp/Controllers/TourController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Turbino.WebApp.Controllers
+{
+    public class TourController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
